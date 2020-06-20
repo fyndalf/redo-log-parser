5445b1f1ba892f7477ea04a7d3b8fad980805164
Finn K <fyndalf@users.noreply.github.com>
2020-06-20 18:04:45 +0200

    Do some very nice linting

--- src/main/scala/parser/package.scala
+++ src/main/scala/parser/package.scala
@@ -12,31 +12,37 @@ package object parser {
     .toFormatter
 
   case class ExtractedLogEntry(
-                       timestamp: LocalDateTime,
-                       tableIdentifier: String,
-                       redoStatement: String
-                     )
+                                timestamp: LocalDateTime,
+                                tableIdentifier: String,
+                                redoStatement: String
+                              )
 
   case class LogEntryWithRedoStatement(
-                                      timestamp: LocalDateTime,
-                                      tableIdentifier: String,
-                                      statement: ParsedStatement,
+                                        timestamp: LocalDateTime,
+                                        tableIdentifier: String,
+                                        statement: ParsedStatement,
                                       )
 
   sealed abstract case class ParsedStatement()
+
   object ParsedStatement {
+
     final case class UpdateStatement(
                                       affectedAttribute: String,
                                       newAttributeValue: String,
                                       oldAttributeValue: String,
                                       rowId: String
-                              )
+                                    )
+
     final case class InsertStatement(
-                                    updatedAttributesAndValues: mutable.HashMap[String, String]
+                                      updatedAttributesAndValues: mutable.HashMap[String, String]
                                     )
+
     final case class DeleteStatement(
-                                    identifyingAttributesAndValues: mutable.HashMap[String, String],
-                                    rowId: String
+                                      identifyingAttributesAndValues: mutable.HashMap[String, String],
+                                      rowId: String
                                     )
+
   }
+
 }
