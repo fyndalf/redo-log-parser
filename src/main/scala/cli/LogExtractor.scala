64e6b65820a71ea26bd794401fe52d6a5c39b149
Finn K <fyndalf@users.noreply.github.com>
2020-07-03 15:50:34 +0200

    Add method to transform row identifiers across record lifetimes

--- src/main/scala/cli/LogExtractor.scala
+++ src/main/scala/cli/LogExtractor.scala
@@ -13,47 +13,42 @@ object LogExtractor extends CommandApp(
 
     val filePath = Opts.argument[Path](metavar = "file")
 
-    // todo: remove this example data
-    val userOpt =
-      Opts
-        .option[String]("target", help = "Person to greet.")
-        .withDefault("world")
-
     val verboseOpt = Opts
       .flag("verbose", help = "Print extra metadata to the console.")
       .orFalse
 
-    (filePath, userOpt, verboseOpt).mapN {
-      (path, user, verbose) =>
+    (filePath, verboseOpt).mapN {
+      (path, verbose) =>
         if (verbose)
           println(s"Getting log file from ${path.toAbsolutePath.toString}")
-        else println(s"Hello $user!")
 
         // todo: make block separator a parameter
         // todo: make date time format a parameter
         val logEntries = FileParser.getAndParseLogFile(path)
         if (verbose) {
           println(s"Read following log entries from ${path.toAbsolutePath.toString}:")
-          logEntries.toList.map(println)
+          logEntries.toList.foreach(println)
         }
         val parsedLogEntries = FileParser.parseLogEntries(logEntries)
         if (verbose) {
           println("Extracted the following statements out of the log entries:")
-          parsedLogEntries.toList.map(println)
+          parsedLogEntries.toList.foreach(println)
+        }
+        val transformedLogEntries = eventExtractor.transformRowIdentifiers(parsedLogEntries)
+        if (verbose) {
+          println("Transformed the following log entries and their row identifiers:")
+          transformedLogEntries.toList.foreach(println)
         }
 
-        //todo: derive unique row ids for each lifetime of a row id
-        // new row id for insert and updates until we see delete or new insert
+      /**
+       * TODO: Add table and column class
+       * A table contains several columns that are discovered in the process
+       * A column contains a flag indicating whether it could be a primary key,
+       * a list of all distinct values found,
+       * and a list of all columns that could be a foreign key that refers to this column
+       */
 
-        /**
-         * TODO: Add table and column class
-         * A table contains several columns that are discovered in the process
-         * A column contains a flag indicating whether it could be a primary key, 
-         * a list of all distinct values found, 
-         * and a list of all columns that could be a foreign key that refers to this column
-         */
-
-        //TODO: Replay the log events and find tables, columns and the relation between them
+      //TODO: Replay the log events and find tables, columns and the relation between them
     }
   }
 )
